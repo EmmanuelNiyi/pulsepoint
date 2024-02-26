3b2a0a8f095c6bd883c09e8f38aedbfee172963c
Fagboye <106874256+Fagboye@users.noreply.github.com>
2024-02-26 18:53:29 +0100

    I am done with working on the calendar integration system

--- donations/utilities/calendar.py
+++ donations/utilities/calendar.py
@@ -11,6 +11,22 @@ from environ import environ
 
 
 
+# defining the path of the env file
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ENV_PATH = f"{BASE_DIR}/keys/.env"
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True)
+)
+
+# reading the env file from the file path
+environ.Env.read_env(env_file=ENV_PATH)
+
+
 
 
 
@@ -26,14 +42,19 @@ def create_service(user):
     Returns:
         An instance of the service for interacting with the Google Calendar API.
     """
+    # defining the scope of the service 
     credentials = None
     scope = 'https://www.googleapis.com/auth/calendar'
+
+    #importing the required environmental variables
+    client_id = env('CLIENT_ID')
+    client_secret = env('CLIENT_SECRET')
 
     # We retrieve user access token and refresh token from the UserToken Model
     user_token = UserToken.objects.get(user=user)
     credentials = Credentials(user_token.access_token, refresh_token=user_token.refresh_token,
-                              token_uri='https://oauth2.googleapis.com/token', client_id= env.CLIENT_ID,
-                              client_secret= env.CLIENT_SECRET)
+                              token_uri='https://oauth2.googleapis.com/token', client_id= client_id,
+                              client_secret= client_secret)
 
     # if the credentials is none or not valid, we try to use refresh token to get a new access token. If it is none,
     # we request for the user authorize the application again.
@@ -43,11 +64,9 @@ def create_service(user):
         else:
             flow = InstalledAppFlow.from_client_secrets_file('Credentials.json', scope)
             credentials = flow.run_local_server(port=0)
-            assert isinstance(credentials, object)
 
-        credentials_json = credentials.to_json()
-        user_token.access_token = credentials_json.token
-        user_token.refresh_token = credentials_json.refresh_token
+        user_token.access_token = credentials.token
+        user_token.refresh_token = credentials.refresh_token
         user_token.save()
 
     # build the service that connects to the Google calendar API
